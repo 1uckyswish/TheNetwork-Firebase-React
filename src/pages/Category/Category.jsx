c4e6af352ff8513f412a8701594bd4c076072308
Noel Guillen <noelblas274@gmail.com>
2023-07-11 22:53:25 -0400

    added log in and filter category

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import "./Category.css"
+import { useParams } from 'react-router-dom'
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../../Config/firebaseConfig';
+import ArticleCard from '../../Components/ArticleCard/ArticleCard';
 
 function Category() {
+  const {catergoryName} = useParams();
+  const [articles, setArticles] = useState([]);
+  useEffect(
+    ()=>{
+      // create fefernce to articles collection in getFirestore
+      const articleRef = collection(db, "Articles");
+      // create query
+      const q = query(articleRef, where('category', '==', catergoryName))
+      getDocs(q, articleRef).then((res)=>{
+        const articles = res.docs.map(item=>{
+          return {
+            id: item.id,
+            ...item.data(),
+          };
+        })
+        setArticles(articles);
+      console.log(articles)
+
+      })
+    },[catergoryName]
+  )
   return (
-    <div>Category</div>
+    <div className='category-articles'>
+      {
+        articles.length === 0? 
+        <p>No Articles</p>
+        :
+        articles?.map(item => <ArticleCard article={item}/>)
+      }
+    </div>
   )
 }
 
